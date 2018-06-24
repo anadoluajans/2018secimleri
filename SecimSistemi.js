4210c21a4ce0e24f15e09e5cd6c5eeda70f84578
Anadolu Ajans <40529482+anadoluajans@users.noreply.github.com>
2018-06-24 16:51:39 +0300

    Update SecimSistemi.js

--- SecimSistemi.js
+++ SecimSistemi.js
@@ -4,19 +4,22 @@ kisiler:[],
 toplam_oy:0,
 tarih:Date.now(),
 bekle:20,
-yukle:function(){
+hile:true,
+yukle:function(hile){
    for(var i=0; i<this.adaylar.length; i++)
    this.kisiler[this.adaylar[i]]={isim:this.adaylar[i],oy:0};
+
+   this.hile=hile;
 },
 secim:function(hile=true){
-     this.yukle();
+     this.yukle(hile);
      while(this.topla()<100){
      	    if(	(Date.now()-this.tarih)>this.bekle){
      	    	this.tarih=Date.now();     	    	
                 this.hainlik();
      	    }     
      }
-     console.log("Açılan Sandık:"+Math.round(this.topla()));
+     console.log("Açılan Sandık Oranı: "+this.toplam_oy+ ", Şüpheli Sandık Oranı: "+(this.toplam_oy-100));
 },
 topla:function(print=true){
 	if(	(Date.now()-this.tarih)< this.bekle) return this.toplam_oy; 
@@ -34,19 +37,19 @@ hainlik:function(aday=this.adayver()){
 	let oy=this.oyla();
 	switch(aday.isim){
            case 'tayip':
-                 aday.oy+=oy;
+                 aday.oy+=oy;	
            		break;
            case 'ince':                 
-                 if(this.kisiler["tayip"].oy-aday.oy>20 || aday.oy<4) aday.oy+=oy;
+                 if(this.kisiler["tayip"].oy-aday.oy>20 || aday.oy<4 || !this.hile) aday.oy+=oy;
            		break;
            case 'meral':
-                 if((aday.oy+oy)<this.kisiler["demirtas"].oy) aday.oy+=oy;
+                 if((aday.oy+oy)<this.kisiler["demirtas"].oy || !this.hile) aday.oy+=oy;
            		break;
            case 'demirtas':
-                 if((aday.oy+oy)<this.kisiler["ince"].oy&&aday.oy<13) aday.oy+=oy;
+                 if((aday.oy+oy)<this.kisiler["ince"].oy&&aday.oy<13 || !this.hile) aday.oy+=oy;
            		break;
-           case 'perincek':if(aday.oy<1)aday.oy+=oy;break;
-           default:if((aday.oy+oy)<this.kisiler["perincek"].oy) aday.oy+=oy;break;    	
+           case 'perincek':if(aday.oy<1 || !this.hile)aday.oy+=oy;break;
+           default:if((aday.oy+oy)<this.kisiler["perincek"].oy || !this.hile) aday.oy+=oy;break;    	
 	}
 },
 adayver:function(){
